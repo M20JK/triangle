3baa5fcf85f0da335698ef6a932a5f7b8429da3e
Mohammed Jawad <al7joker111@gmail.com>
2019-09-21 16:55:13 +0300

    triangle code

---
+++ maun.js
@@ -0,0 +1,11 @@
+let side1 = document.getElementById("side1") ;
+let side2 = document.getElementById("side2") ;
+let side3 = document.getElementById("side3") ;
+
+
+function result() {
+    if (side1 != null && side2 != null && side3 != null  ) {
+        
+        document.getElementById("resulty")innerHTML = "The Area of this triangle is : " + 
+    }
+}
